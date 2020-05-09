025a4fab94d26454f171e090221e95ce68e4fdea
Ninad Rajandekar <ninadsidh@gmail.com>
2020-05-09 14:50:00 +0530

    Project preliminary completed

--- routes/gigs.js
+++ routes/gigs.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op; 
 
 // Get gig list
 router.get('/', (req, res) => 
     Gig.findAll()
-        .then(gigs => {
-            res.render('gigs', {
+        .then(gigs => res.render('gigs', {
                 gigs
-            });
-        })
+        }))
         .catch(err => console.log(err)));
 
 // Display add gig form
@@ -20,29 +20,67 @@ router.get('/add', (req,res) => {
 
 // Add a gig
 router.post('/add', (req,res) => {
-    const data = {
-        title: 'simple wordpress',
-        technology: 'wordpress, html, css',
-        budget: '$1000',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-        contact_email: 'user2@gmail.com'
+    
+    let {title, technology, budget, description, contact_email} = req.body;
+    let errors = [];
+
+    //Validate fields
+    if (!title) {
+        errors.push({text: 'Please add a title'});
+    }
+    if (!technology) {
+        errors.push({text: 'Please add some techonologies'});
+    }
+    if (!description) {
+        errors.push({text: 'Please add a description'});
+    }
+    if (!contact_email) {
+        errors.push({text: 'Please add a contact Email'});
     }
 
-    let {title, technology, budget, description, contact_email} = data;
-    
-    //Insert into table
-    Gig.create({
-        title,
-        technology,
-        description,
-        budget,
-        contact_email
-    })
-        .then(gig => res.redirect('/gigs'))
-        .catch(err => console.log(err));
+    //Check for errors
+    if (errors.length > 0){
+        res.render('add', {
+            errors,
+            title,
+            technology,
+            description,
+            budget,
+            contact_email
+        })
+    } else{
+        if (!budget)    {
+            budget = 'Unknown';
+        } else {
+            budget = `$${budget}`;
+        }
+        
+        // Make lower case and remove space after comma
+        technology = technology.toLowerCase().replace(/, /g, ',');
+
+        //Insert into table
+        Gig.create({
+            title,
+            technology,
+            description,
+            budget,
+            contact_email
+        })
+            .then(gig => res.redirect('/gigs'))
+            .catch(err => console.log(err));
+    }
+
+  
 });
 
-
-
+// Search for gigs
+router.get('/search', (req, res)=> {
+    let {term} = req.query;
+    //Make lowercase
+    term = term.toLowerCase();
+    Gig.findAll({ where: {technology: { [Op.like]: '%'+ term + '%' } } })
+        .then(gigs => res.render('gigs', { gigs }))
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
